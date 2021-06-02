50e18831aec443b21c938846a24c4b837f8376fd
nomiad1 <engineer.nomanahmed@gmail.com>
2021-06-03 04:11:41 +0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $('.home-banner').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        dots: true,
+        fade: true,
+        speed: 1800,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+});
